8527f0a3280c1051ee01b4160f5b2be3e6606a9c
Ashic Mahtab (MC) <ashic@live.com>
2015-01-09 12:18:36 +0000

    added support for optional separate source.xml file

--- src/config-transform/config-transform/Program.cs
+++ src/config-transform/config-transform/Program.cs
@@ -8,18 +8,20 @@ namespace config_transform
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
-                throw new ArgumentException("Usage: config-transform config.xml transform.xml");
+            if (args.Length != 2 && args.Length != 3)
+                throw new ArgumentException("Usage: config-transform config.xml transform.xml [original.xml]. If original.xml is not provided, config.xml is overwritten.");
 
             var transformer = new XmlTransformableDocument { PreserveWhitespace = true };
 
-            if (File.Exists(args[0]) == false)
-                throw new FileNotFoundException(string.Format("Config file {0} could not be found.", args[0]));
+            var sourceIndex = args.Length == 3 ? 2 : 0;
+
+            if (File.Exists(args[sourceIndex]) == false)
+                throw new FileNotFoundException(string.Format("Config file {0} could not be found.", args[sourceIndex]));
 
             if (File.Exists(args[1]) == false)
                 throw new FileNotFoundException(string.Format("Transform file {0} could not be found.", args[1]));
 
-            transformer.Load(args[0]);
+            transformer.Load(args[sourceIndex]);
 
             var transform = new XmlTransformation(args[1]);
 
